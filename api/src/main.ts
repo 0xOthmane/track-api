1deed985000d1db840d755b9e2376dd8a3a3c7e7
0xOthmane <0xothodor@proton.me>
2026-05-20 08:15:20 +0000

    feat: update User model to use Role enum and set default role; enhance auth role handling

--- api/src/main.ts
+++ api/src/main.ts
@@ -5,6 +5,7 @@ import { Logger } from 'nestjs-pino';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     bodyParser: false,
+    bufferLogs: true,
   });
   app.useLogger(app.get(Logger));
   await app.listen(process.env.PORT ?? 3000);
