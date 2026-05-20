6c1f1583f42c8b5506d054497901614d7590e43f
0xOthmane <0xothodor@proton.me>
2026-05-20 10:39:59 +0000

    feat: add openAPI plugin and update disabledPaths in auth configuration

--- api/src/lib/auth.ts
+++ api/src/lib/auth.ts
@@ -2,6 +2,7 @@ import { betterAuth } from 'better-auth';
 import { PrismaPg } from '@prisma/adapter-pg';
 import { prismaAdapter } from 'better-auth/adapters/prisma';
 import { PrismaClient } from '../generated/prisma/client';
+import { openAPI } from 'better-auth/plugins';
 import { env } from '../config/env.config';
 
 const prisma = new PrismaClient({
@@ -21,4 +22,34 @@ export const auth = betterAuth({
       },
     },
   },
+  disabledPaths: [
+    '/sign-up/email',
+    '/forget-password',
+    // '/reset-password',
+    '/update-user',
+    '/delete-user',
+    '/change-email',
+    '/change-password',
+    '/list-sessions',
+    '/revoke-session',
+    '/revoke-sessions',
+    '/revoke-other-sessions',
+    '/sign-in/social',
+    '/get-session',
+    '/verify-password',
+    '/verify-email',
+    '/send-verification-email',
+    '/update-session',
+    '/request-password-reset',
+    '/link-social',
+    '/list-accounts',
+    '/delete-user/callback',
+    '/unlink-account',
+    '/refresh-token',
+    '/get-access-token',
+    '/account-info',
+    '/ok',
+    '/error',
+  ],
+  plugins: [openAPI()],
 });
