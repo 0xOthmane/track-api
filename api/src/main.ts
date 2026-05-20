8d6ebbb2c07379c5fd64ee7f10452a863c234b7b
0xOthmane <0xothodor@proton.me>
2026-05-20 08:31:26 +0000

    feat: add Swagger support for API documentation

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,12 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { Logger } from 'nestjs-pino';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     bodyParser: false,
     bufferLogs: true,
   });
+  const config = new DocumentBuilder()
+    .setTitle('Track API')
+    .setDescription('API documentation for Track API')
+    .setVersion('0.1')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, documentFactory);
   app.useLogger(app.get(Logger));
   await app.listen(process.env.PORT ?? 3000);
 }
